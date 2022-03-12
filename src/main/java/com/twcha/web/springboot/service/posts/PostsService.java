cc5e708f2a36f97a9c6e877b15338e64069e9f29
chartw <12161800@inha.edu>
2022-03-13 02:12:31 +0900

    수정/조회 API 생성 및 테스트

--- src/main/java/com/twcha/web/springboot/service/posts/PostsService.java
+++ src/main/java/com/twcha/web/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.twcha.web.springboot.service.posts;
 
+import com.twcha.web.springboot.domain.posts.Posts;
 import com.twcha.web.springboot.domain.posts.PostsRepository;
+import com.twcha.web.springboot.web.dto.PostsResponseDto;
 import com.twcha.web.springboot.web.dto.PostsSaveRequestDto;
+import com.twcha.web.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +18,23 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    @Transactional
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        return new PostsResponseDto(entity);
     }
 }
