4f9e714e5b9cd02b613c07caf0f8169ee24ef4d8
chartw <12161800@inha.edu>
2022-03-13 00:48:32 +0900

    JPA, H2 기본 셋팅

---
+++ src/main/java/com/twcha/web/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.twcha.web.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts,Long> {
+}
